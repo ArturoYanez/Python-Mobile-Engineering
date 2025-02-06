17d39a4205b4a62fb38d8011f435df9b307c3967
ArturoYanez <Arturo_w_0@hotmail.com>
2025-02-06 15:45:34 -0400

    fix: calculator.py flake8 linter errors fixes

--- Basic-Skills/calculator/calculator.py
+++ Basic-Skills/calculator/calculator.py
@@ -17,53 +17,58 @@ exit = False
 
 
 def sum(a, b):
-  result = a + b
-  return result
+    result = a + b
+    return result
   
   
 def sub(a, b):
-  result = a - b
-  return result
+    result = a - b
+    return result
   
   
 def mul(a, b):
-  result = a * b
-  return
+    result = a * b
+    return
   
   
 def div(a, b):
-  result = a / b
-  return result
+    result = a / b
+    return result
 
 
 while exit is False:
-  print('——————>')
-  print('''Menu
-  1 - Sum
-  2 - Subtraction
-  3 - Multiplication
-  4 - Division
-  5 - Exit
+    print('——————>')
+    print('''Menu
+    1 - Sum
+    2 - Subtraction
+    3 - Multiplication
+    4 - Division
+    5 - Exit
   ''')
   
-  print('——————>')
-  option = int(input('Enter the operation you want to perform [1 - 5]: '))
-  if option == 1:
-    a = int(input('First number to operate: '))
-    b = int(input('Second number to operate: '))
-    print(f'Result = {sum(a, b)}')
-  elif option == 2:
-    a = int(input('First number to operate: '))
-    b = int(input('Second number to operate: '))
-    print(f'Result = {sub(a, b)}')
-  elif option == 3:
-    a = int(input('First number to operate: '))
-    b = int(input('Second number to operate: '))
-    print(f'Result = {mul(a, b)}')
-  elif option == 4:
-    a = int(input('First number to operate: '))
-    b = int(input('Second number to operate: '))
-    print(f'Result = {div(a, b)}')
-  elif option == 5:
-    exit = True
-    print(' Thanks to test our program, see ya later')
+    print('——————>')
+    option = int(input('Enter the operation you want to perform [1 - 5]: '))
+    if option == 1:
+        a = int(input('First number to operate: '))
+        b = int(input('Second number to operate: '))
+        print(f'Result = {sum(a, b)}')
+
+    elif option == 2:
+        a = int(input('First number to operate: '))
+        b = int(input('Second number to operate: '))
+        print(f'Result = {sub(a, b)}')
+
+    elif option == 3:
+        a = int(input('First number to operate: '))
+        b = int(input('Second number to operate: '))
+        print(f'Result = {mul(a, b)}')
+
+    elif option == 4:
+        a = int(input('First number to operate: '))
+        b = int(input('Second number to operate: '))
+        print(f'Result = {div(a, b)}')
+
+    elif option == 5:
+        exit = True
+        print(' Thanks to test our program, see ya later')
+
